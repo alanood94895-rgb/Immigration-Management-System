3b32f6fa2a7ba030d411e75d91a2bd343185a54f
CompanyProfile <alanood94895@gmail.com>
2026-06-15 14:45:14 +0400

    Bad Request validation failures

--- src/main/java/com/example/tra/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/tra/Exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,21 @@
 package com.example.tra.Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity <ErrorResponse> handleValidationException (ValidationException ex){
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation Error",
+                ex.getMessage()
+        );
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
 
 }
