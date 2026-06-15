0cc9b45983eb0aac3f260598d5a28907afba2939
CompanyProfile <alanood94895@gmail.com>
2026-06-15 09:36:06 +0400

    Add method for person class

--- src/main/java/com/example/tra/Person.java
+++ src/main/java/com/example/tra/Person.java
@@ -1,5 +1,13 @@
 package com.example.tra;
 
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person {
+    String firstName;
+    String lastName;
+    String gender;
+    String phoneNumber;
+    String email;
 
 }
