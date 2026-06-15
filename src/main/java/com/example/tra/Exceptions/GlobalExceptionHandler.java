635c8afada4a40aa867433f06c23ebcbbda1ff85
CompanyProfile <alanood94895@gmail.com>
2026-06-15 14:46:49 +0400

    Not Found  Applicant does not exist

--- src/main/java/com/example/tra/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/tra/Exceptions/GlobalExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler
+    @ExceptionHandler (ValidationException.class)
     public ResponseEntity <ErrorResponse> handleValidationException (ValidationException ex){
         ErrorResponse error = new ErrorResponse(
                 HttpStatus.BAD_REQUEST.value(),
@@ -15,6 +15,15 @@ public class GlobalExceptionHandler {
                 ex.getMessage()
         );
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(ApplicantNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleApplicantNotFoundException(ApplicantNotFoundException ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                "Not Found",
+                ex.getMessage()
+        );
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
 
