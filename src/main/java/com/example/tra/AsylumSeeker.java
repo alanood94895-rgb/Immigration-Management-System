2d7d1cce493a9fce0dc12d1273fcddc59bb94dd9
CompanyProfile <alanood94895@gmail.com>
2026-06-15 10:15:48 +0400

    Create AsylumSeeker Class

--- src/main/java/com/example/tra/AsylumSeeker.java
+++ src/main/java/com/example/tra/AsylumSeeker.java
@@ -1,8 +1,14 @@
 package com.example.tra;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AsylumSeeker extends Applicant{
 
     private String countryOfOrigin;
