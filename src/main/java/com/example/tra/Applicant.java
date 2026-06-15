aa252218e14bae434377e529e5799250baa46101
CompanyProfile <alanood94895@gmail.com>
2026-06-15 09:48:20 +0400

    Create Applicant class

--- src/main/java/com/example/tra/Applicant.java
+++ src/main/java/com/example/tra/Applicant.java
@@ -1,4 +1,12 @@
 package com.example.tra;
 
-public class Applicant {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Applicant extends Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 }
