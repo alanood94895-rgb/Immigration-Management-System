5165aa36d23273f3f031ba3217fa0b00a258c613
CompanyProfile <alanood94895@gmail.com>
2026-06-15 10:34:09 +0400

    Create Interview Class

---
+++ src/main/java/com/example/tra/Interview.java
@@ -0,0 +1,26 @@
+package com.example.tra;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Interview {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Applicant applicant;
+
+    @ManyToOne
+    private ImmigrationOfficer officer;
+
+    private String interviewDate;
+    private String status;
+    private String purpose;
+}
