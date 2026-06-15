190f962ca8bcb1c322227704e94192bcfd83131c
CompanyProfile <alanood94895@gmail.com>
2026-06-15 10:28:55 +0400

    Create ImmigrationCenter Class

---
+++ src/main/java/com/example/tra/ImmigrationCenter.java
@@ -0,0 +1,26 @@
+package com.example.tra;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+    public class ImmigrationCenter {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private String name;
+        private String locationCountry;
+        private String type;
+        private int dailyCapacity;
+
+        @OneToMany(mappedBy = "center")
+        private List<ImmigrationOfficer> officers;
+}
