c80d3ced0340dd4d0068e5b8296c246bfd9a8107
Alanoud <alanood94895@gmail.com>
2026-06-16 09:37:46 +0400

    Create OfficerRepository Class

--- src/main/java/com/example/tra/Repositories/OfficerRepository.java
+++ src/main/java/com/example/tra/Repositories/OfficerRepository.java
@@ -3,5 +3,10 @@ package com.example.tra.Repositories;
 import com.example.tra.Entities.ImmigrationOfficer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OfficerRepository extends JpaRepository<ImmigrationOfficer, Long> {
+
+    List<ImmigrationOfficer> findByOfficerRank(String officerRank);
+
 }
