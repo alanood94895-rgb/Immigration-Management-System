9df305263d75c009a2734d8c8d1a3025a4eeca38
Alanoud <alanood94895@gmail.com>
2026-06-16 14:18:04 +0400

    Create InterviewController Class

--- src/main/java/com/example/tra/Controllers/InterviewController.java
+++ src/main/java/com/example/tra/Controllers/InterviewController.java
@@ -1,10 +1,9 @@
 package com.example.tra.Controllers;
 
 import com.example.tra.Entities.Interview;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/interviews")
