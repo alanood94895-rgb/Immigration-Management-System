0239a2e7e5d36ea67b5606dd98707c1361d7cc08
Alanoud <alanood94895@gmail.com>
2026-06-16 11:37:03 +0400

    Create CenterController class

--- src/main/java/com/example/tra/Controllers/CenterController.java
+++ src/main/java/com/example/tra/Controllers/CenterController.java
@@ -1,9 +1,26 @@
 package com.example.tra.Controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.tra.Entities.ImmigrationCenter;
+import com.example.tra.Repositories.CenterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/centers")
 public class CenterController {
+
+    @Autowired
+    private CenterRepository centerRepository;
+
+    @PostMapping
+    public ImmigrationCenter createCenter(@RequestBody ImmigrationCenter center) {
+        return centerRepository.save(center);
+    }
+
+    @GetMapping("/{id}")
+    public ImmigrationCenter getCenter(@PathVariable Long id) {
+        return centerRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Center not found"));
+    }
+
 }
