d6ffa1042eefbdf431418d50aaab8d0424a6079f
Alanoud <alanood94895@gmail.com>
2026-06-16 11:40:12 +0400

    create ApplicantController class

--- src/main/java/com/example/tra/Controllers/ApplicantController.java
+++ src/main/java/com/example/tra/Controllers/ApplicantController.java
@@ -1,11 +1,13 @@
 package com.example.tra.Controllers;
 
 import com.example.tra.Entities.Applicant;
+import com.example.tra.Entities.AsylumSeeker;
 import com.example.tra.Repositories.ApplicantRepository;
 import com.example.tra.Services.ApplicantService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/applicants")
