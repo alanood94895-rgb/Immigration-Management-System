f5fccce0426792479b09f18407eb96c5e2f6ae24
Alanoud <alanood94895@gmail.com>
2026-06-17 11:55:40 +0400

    create VisaApplicationService class

--- src/main/java/com/example/tra/Controllers/VisaController.java
+++ src/main/java/com/example/tra/Controllers/VisaController.java
@@ -1,8 +1,10 @@
 package com.example.tra.Controllers;
 
+import com.example.tra.DTOs.VisaApplicationDTO;
 import com.example.tra.Entities.VisaApplication;
 import com.example.tra.Services.VisaApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
