9ca97a1ecbd8c1f60e85a2bfe84d7081356e262a
Alanoud <alanood94895@gmail.com>
2026-06-17 10:49:58 +0400

    Create InterviewDTO class

--- src/main/java/com/example/tra/DTOs/InterviewDTO.java
+++ src/main/java/com/example/tra/DTOs/InterviewDTO.java
@@ -7,4 +7,10 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class InterviewDTO {
+    private Long id;
+    private String interviewDate;
+    private String status;
+    private String purpose;
+    private Long applicantId;
+    private Long officerId;
 }
