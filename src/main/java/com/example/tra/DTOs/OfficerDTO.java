d0a6db0feac79c4a346215b48711d7833324c2c7
Alanoud <alanood94895@gmail.com>
2026-06-17 10:54:27 +0400

    Create OfficerDTO class

--- src/main/java/com/example/tra/DTOs/OfficerDTO.java
+++ src/main/java/com/example/tra/DTOs/OfficerDTO.java
@@ -6,4 +6,10 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class OfficerDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String badgeNumber;
+    private String officerRank;
+    private boolean active;
 }
