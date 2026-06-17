dcedcf37f84c0a668ba153fd93e07ab66d8f761a
Alanoud <alanood94895@gmail.com>
2026-06-17 11:51:28 +0400

    Create InterviewController Class

--- src/main/java/com/example/tra/Controllers/InterviewController.java
+++ src/main/java/com/example/tra/Controllers/InterviewController.java
@@ -1,8 +1,10 @@
 package com.example.tra.Controllers;
 
+import com.example.tra.DTOs.InterviewDTO;
 import com.example.tra.Entities.Interview;
 import com.example.tra.Services.InterviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
