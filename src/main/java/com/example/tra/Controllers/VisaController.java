67437879b58968486b5fcfb1409e10fd7fff023b
Alanoud <alanood94895@gmail.com>
2026-06-17 11:54:37 +0400

    create VisaApplicationService class

--- src/main/java/com/example/tra/Controllers/VisaController.java
+++ src/main/java/com/example/tra/Controllers/VisaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/visas")
+@RequestMapping("visas")
 public class VisaController {
     @Autowired
     private VisaApplicationService visaService;
