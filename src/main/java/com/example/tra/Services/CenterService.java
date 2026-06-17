9d61d1d747ac46e351a32bd4dec1c7309a8d7c91
Alanoud <alanood94895@gmail.com>
2026-06-17 11:18:00 +0400

    Create CenterService Class

--- src/main/java/com/example/tra/Services/CenterService.java
+++ src/main/java/com/example/tra/Services/CenterService.java
@@ -4,4 +4,29 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CenterService {
+    @Autowired
+    CenterRepository centerRepository;
+
+
+    public ImmigrationCenter createCenter(ImmigrationCenter center){
+        if (center.getName() == null || center.getName().isBlank()){
+            throw Exceptions.badRequest("Center name is required");
+        }
+        if (center.getLocationCountry() == null || center.getLocationCountry().isBlank()){
+            throw Exceptions.badRequest("Location country is required");
+        }
+        if (center.getType() == null || center.getType().isBlank()){
+            throw Exceptions.badRequest("Center Type is required");
+        }
+        if (center.getDailyCapacity() <= 0){
+            throw Exceptions.badRequest("Daily Capacity must be grater than 0");
+        }
+        return centerRepository.save(center);
+    }
+
+    public ImmigrationCenter getCenterByID(Long id){
+        return centerRepository.findById(id)
+                .orElseThrow(() -> Exceptions.notFound("Center not found with id: " + id));
+    }
+
 }
